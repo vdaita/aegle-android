84263617ccdac2d49398267601d99e2f515f456a
Vijay Daita <hello@daita.com>
2020-08-14 22:01:18 -0400

    initial ui

---
+++ app/src/main/java/com/daita/aegle/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.daita.aegle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/com/daita/aegle/UserHomepage.kt
@@ -0,0 +1,11 @@
+package com.daita.aegle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserHomepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_homepage)
+    }
+}
